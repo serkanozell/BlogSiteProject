0c65ba992242ecd8ec62f3d5c5c5ec69ba6c8d5c
Serkan OZEL <serkan.ozel@bilgeadam.com>
2022-07-11 18:50:47 +0300

    Notification Page Added Successfully

--- WebUI/Controllers/NotificationController.cs
+++ WebUI/Controllers/NotificationController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -10,9 +11,22 @@ namespace WebUI.Controllers
     [AllowAnonymous]
     public class NotificationController : Controller
     {
+        INotificationService _notificationService;
+
+        public NotificationController(INotificationService notificationService)
+        {
+            _notificationService = notificationService;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult AllNotification()
+        {
+            var result = _notificationService.GetList();
+            return View(result);
         }
     }
 }
