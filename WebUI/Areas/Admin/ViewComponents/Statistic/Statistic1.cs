df709800c7094af7d03d1ce872ea94071fec051c
Serkan OZEL <serkan.ozel@bilgeadam.com>
2022-07-13 17:26:27 +0300

    Theme Edit

---
+++ WebUI/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -0,0 +1,30 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebUI.Areas.Admin.ViewComponents.Statistic
+{
+    public class Statistic1 : ViewComponent
+    {
+        IBlogService _blogService;
+
+        public Statistic1(IBlogService blogService)
+        {
+            _blogService = blogService;
+        }
+
+        Context context = new Context();
+
+        public IViewComponentResult Invoke()
+        {
+            var result = _blogService.GetList();
+            ViewBag.messageCount = context.Contacts.Count();
+            ViewBag.commentCount = context.Comments.Count();
+            return View(result);
+        }
+    }
+}
---
+++ WebUI/Areas/Admin/ViewComponents/Statistic/Statistic2.cs
@@ -0,0 +1,29 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebUI.Areas.Admin.ViewComponents.Statistic
+{
+    public class Statistic2 : ViewComponent
+    {
+        IBlogService _blogService;
+
+        public Statistic2(IBlogService blogService)
+        {
+            _blogService = blogService;
+        }
+
+        Context context = new Context();
+        public IViewComponentResult Invoke()
+        {
+            var result = _blogService.GetList().Last();
+            ViewBag.messageCount = context.Contacts.Count();
+            ViewBag.commentCount = context.Comments.Count();
+            return View(result);
+        }
+    }
+}
