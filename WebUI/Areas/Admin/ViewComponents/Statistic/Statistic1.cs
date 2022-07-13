7243101646981e14d9ac3de898c5d62dbfc3904c
Serkan OZEL <serkan.ozel@bilgeadam.com>
2022-07-14 00:59:22 +0300

    weatherapi - xml format statistic added succesfully

--- WebUI/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
+++ WebUI/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace WebUI.Areas.Admin.ViewComponents.Statistic
 {
@@ -24,6 +25,11 @@ namespace WebUI.Areas.Admin.ViewComponents.Statistic
             var result = _blogService.GetList();
             ViewBag.messageCount = context.Contacts.Count();
             ViewBag.commentCount = context.Comments.Count();
+
+            string api = "3a9ff604fd9b6a270336fbfc4b1c993b";
+            string connection = "https://api.openweathermap.org/data/2.5/weather?q=istanbul&mode=xml&lang=tr&units=metric&appid=" + api;
+            XDocument document = XDocument.Load(connection);
+            ViewBag.temp = document.Descendants("temperature").ElementAt(0).Attribute("value").Value;
             return View(result);
         }
     }
