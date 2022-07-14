925c1fb5eba01f8472f5592865df079bccf1205f
Serkan OZEL <serkan.ozel@bilgeadam.com>
2022-07-14 03:05:58 +0300

    Charts Added Successfully

---
+++ WebUI/Areas/Admin/Controllers/ChartController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebUI.Areas.Admin.Models;
+
+namespace WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ChartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CategoryChart()
+        {
+            List<CategoryClass> categoryclasses = new List<CategoryClass>();
+
+            categoryclasses.Add(new CategoryClass
+            {
+                categoryname = "Technology",
+                categorycount = 10,
+            });
+            categoryclasses.Add(new CategoryClass
+            {
+                categoryname = "Software",
+                categorycount = 7,
+
+            });
+            categoryclasses.Add(new CategoryClass
+            {
+                categoryname = "Sport",
+                categorycount = 14,
+            });
+            categoryclasses.Add(new CategoryClass
+            {
+                categoryname = "Social",
+                categorycount = 34,
+            });
+            return Json(new { jsonlist = categoryclasses });
+        }
+    }
+}
