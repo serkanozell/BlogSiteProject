f898350625c53d6d4e912ba77230ce9977084b1d
Serkan OZEL <serkan.ozel@bilgeadam.com>
2022-07-15 01:37:03 +0300

    WebUI and API Connection Added Successfully

---
+++ WebUI/Controllers/EmployeeTestController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using WebUI.Models;
+
+namespace WebUI.Controllers
+{
+    public class EmployeeTestController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var httpClient = new HttpClient();
+            var responseMessage = await httpClient.GetAsync("https://localhost:44301/api/Employee");
+            var jsonString = await responseMessage.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<List<EmployeeModel>>(jsonString);
+            return View(result);
+        }
+
+        public IActionResult AddEmployee()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddEmployee(EmployeeModel employeeModel)
+        {
+            var httpClient = new HttpClient();
+            var jsonEmployee = JsonConvert.SerializeObject(employeeModel);
+            StringContent stringContent = new StringContent(jsonEmployee, Encoding.UTF8, "application/json");
+            var responseMessage = await httpClient.PostAsync("https://localhost:44301/api/Employee", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(employeeModel);
+            }
+        }
+    }
+}
---
+++ WebUI/Models/EmployeeModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebUI.Models
+{
+    public class EmployeeModel
+    {
+        public int EmployeeID { get; set; }
+        public string EmployeeName { get; set; }
+    }
+}
